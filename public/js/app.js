589987e6592de9e58077e89c0c3e5ccc95c54c9c
Satyendra Kumar <satyendravns121@gmail.com>
2021-09-29 03:05:02 -0700

    Fixed bug

--- public/js/app.js
+++ public/js/app.js
@@ -11,17 +11,17 @@
 
 // })
 
-fetch('/weather?address=').then((response)=>{
-    response.json().then((data)=>{
-        if(data.error){
-            console.log(data.error)
-        }
-        else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('/weather?address=').then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error){
+//             console.log(data.error)
+//         }
+//         else{
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
@@ -37,7 +37,7 @@ weatherForm.addEventListener('submit',(e)=>{
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
     
-fetch(`http://localhost:3000/weather?address=${location}`).then((response)=>{
+fetch('/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             // console.log(data.error)
